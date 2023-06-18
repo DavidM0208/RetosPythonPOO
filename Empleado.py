4ecb79e412c0f8814f49691c9edb93e755e8c849
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-06-18 16:29:53 -0500

    Se añadieron condiciones

--- Empleado.py
+++ Empleado.py
@@ -15,9 +15,9 @@ class Empleado():
         self.cargo = input('Ingrese su cargo: ')
         self.valorhora = int(input('Ingrese el valor a pagar por hora de trabajo: '))
         self.departamento = input('Ingrese el area en el que trabaja: ')    
+    def mostrarPersona(self):
+        print(f'Nombre: {self.nombre} \nApellido: {self.apellido} \nTipo de documento: {self.tipoDoc} \nDocumento: {self.documento} \nHoras trabajadas: {self.horastrabajadas} \nValor de la hora: {self.valorhora}')
     def calcularHonorarios(self):
         self.horastrabajadas = int(input('Ingrese las horas trabajadas: '))
         honorarios = (self.valorhora * self.horastrabajadas) - 0.966
         print(f'Sus honorarios correspondientes son: {honorarios}')
-    def mostrarPersona(self):
-        print(f'Nombre: {self.nombre} \nApellido: {self.apellido} \nTipo de documento: {self.tipoDoc} \nDocumento: {self.documento} \nHoras trabajadas: {self.horastrabajadas}')
--- Inicio.py
+++ Inicio.py
@@ -1,16 +1,41 @@
 from Persona import Persona
 from Empleado import Empleado
 
+op = 1
+
 # pruebaPersona = Persona ('David', 'Ramos', 'Masculino', 'Cedula', 1000511858, 90, 1.92, 24)
 # pruebaPersona.mostrarPersona()
 
-empleado = Empleado()
-empleado.pedirDatos()
-empleado.calcularHonorarios()
-empleado.mostrarPersona()
+print('¡Bienvenido!')
 
-persona = Persona()
-persona.pedirDatos()
-persona.mostrarPersona()
-persona.calcularImc()
-persona.mayorEdad()
+while op == 1:
+        
+    print('1. Para registrarse como empleado.')
+    print('2. Para registrarse como persona ordinaria.')
+    print('3. Para salir.')
+
+    opcion = int(input('Seleciones un opción: '))
+
+    if opcion == 1:
+        print('Ingrese sus datodo correcpondientes: \n')
+        empleado = Empleado()
+        empleado.pedirDatos()
+        empleado.calcularHonorarios()
+        empleado.mostrarPersona()
+
+    elif opcion == 2:
+        print('Ingrese sus datodo correcpondientes: \n')
+        persona = Persona()
+        persona.pedirDatos()
+        persona.mostrarPersona()
+        persona.calcularImc()
+        persona.mayorEdad()
+
+    elif opcion == 3:
+        print('Gracias por usar nuestros servicios')
+        break
+
+    else:
+        print('Opción no valida.')
+
+    op =  int(input('1. Para añadir persona/empleado. \n2. Para salir. \n'))
