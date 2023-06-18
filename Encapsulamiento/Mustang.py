c9500fca88215b0093a1fbc007b1c6b8f041bb6b
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-06-18 16:51:38 -0500

    Se cambiaron los archivos

--- Encapsulamiento/Mustang.py
+++ Encapsulamiento/Mustang.py
@@ -1,4 +1,13 @@
 from Carro import Carro
 
-mustang = Carro('Ford', 'Mustang', 5000000)
+# Creamos el objeto, instanciando nuestra clase Carro. 
+
+mustang = Carro('Ford', 'Mustang', 50000)
+
+# Accedemos al precio del objeto con el método.
+
 print(mustang.get_precio())
+
+# En consola debe de salir el precio que ya pusimos. 50000
+
+# Al intentar imprimir el precio de esta manera "print(mustang.__precio)", nos saltara un mensaje de error, debido a que es privado. 
